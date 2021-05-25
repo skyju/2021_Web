a19c0a9c5dc803a9e10a98824e0cc8ee9aab1b32
skyju <77267141+skyju@users.noreply.github.com>
2021-05-25 09:55:31 +0900

    new

--- MyProject/src/ch01/Member.java
+++ MyProject/src/ch01/Member.java
@@ -4,12 +4,6 @@ public class Member {
 	
 	public static void main(String[] args) {
 		System.out.println("곽민주입니다.");
-		
-		String name;
-		int age;
-		double height;
-		boolean hasBook;
-		
 	}
 
 }
---
+++ MyProject/src/ch02/Member.java
@@ -0,0 +1,19 @@
+package ch02;
+
+public class Member {
+
+	public static void main(String[] args) {
+		
+		// 변수 선언
+		// 데이터 타입 식별 이름
+		// 데이터 타입 > 내가 다루어야 하는 데이터의 특징에 의하여 결정
+		// 기본형 8가지 타입 > 정수형, 실수형, bool, 문자
+		
+		String name;
+		int age;
+		double height;
+		boolean hasBook;
+
+	}
+
+}
