393c36fc03539b0bb52e8658482422132b9e4838
skyju <77267141+skyju@users.noreply.github.com>
2021-05-25 14:58:28 +0900

    Update Calculator.java

--- MyProject/src/ch03/Calculator.java
+++ MyProject/src/ch03/Calculator.java
@@ -17,6 +17,7 @@ import java.util.Scanner;
 public class Calculator {
 	
 	public static String inputValue() {
+		//java.util.Scanner
 		Scanner scanner = new Scanner(System.in);
 		String input = scanner.nextLine();
 		return input;
@@ -35,7 +36,7 @@ public class Calculator {
 	}
 	
 	double divide(int x, int y) {
-		return x/y;
+		return (double)x/y;
 	}
 	
 	// 클래스 이름 : 대문자 시작, 케멀 케이스
@@ -45,11 +46,11 @@ public class Calculator {
 	float pi2 = 3.14f;
 	final float PI = 3.14f;
 	
-	float circumference(int r) {
+	float circumference(float r) {
 		return 2*PI*r;
 	}
 	
-	float areaOfCircle(int r) {
+	float areaOfCircle(float r) {
 		return PI*r*r;
 	}
 	
@@ -58,25 +59,20 @@ public class Calculator {
 		try {
 		Calculator cal = new Calculator();
 		
-		System.out.println("연산할 두 값을 입력해주세요.");
-		int a = Integer.parseInt(inputValue());
-		int b = Integer.parseInt(inputValue());
-		long c1 = cal.add(a,b);
-		long c2 = cal.substract(a,b);
-		long c3 = cal.multiply(a,b);
-		double c4 = cal.divide(a,b);
-		System.out.printf("%d + %d = %d\n",a,b,c1);
-		System.out.printf("%d - %d = %d\n",a,b,c2);
-		System.out.printf("%d * %d = %d\n",a,b,c3);
-		System.out.printf("%d / %d = %g\n",a,b,c4);
+		System.out.println("연산 할 두 값을 입력해주세요.\n첫 번째 숫자를 입력해주세요.");
+		int num1 = Integer.parseInt(inputValue());
+		System.out.println("두 번째 숫자를 입력해주세요.");
+		int num2 = Integer.parseInt(inputValue());
+		System.out.printf("%d + %d = %d\n",num1,num2,cal.add(num1,num2));
+		System.out.printf("%d - %d = %d\n",num1,num2,cal.substract(num1,num2));
+		System.out.printf("%d * %d = %d\n",num1,num2,cal.multiply(num1,num2));
+		System.out.printf("%d / %d = %g\n",num1,num2,cal.divide(num1,num2));
 		
-		System.out.println();
+		System.out.println("------------------------");
 		System.out.println("원의 반지름을 입력해주세요.");
-		int a1 = Integer.parseInt(inputValue());
-		float c5 = cal.circumference(a1);
-		System.out.printf("원의 둘레는 %g 입니다.\n",c5);
-		float c6 = cal.areaOfCircle(a1);
-		System.out.printf("원의 넓이는 %g 입니다.\n",c6);
+		float r = Float.parseFloat(inputValue());
+		System.out.printf("원의 둘레는 %g 입니다.\n",cal.circumference(r));
+		System.out.printf("원의 넓이는 %g 입니다.\n",cal.areaOfCircle(r));
 		
 		} catch(ArithmeticException e) {
 			System.out.println("0으로는 나눌 수 없습니다.");
