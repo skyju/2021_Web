78835ecb0e81369531be993dc1bb60a0dd174d11
skyju <77267141+skyju@users.noreply.github.com>
2021-05-25 15:53:03 +0900

    Update Member.java

--- MyProject/src/ch04/Member.java
+++ MyProject/src/ch04/Member.java
@@ -1,5 +1,7 @@
 package ch04;
 
+import java.util.*;
+
 //1. 앞에서 만든 Member 클래스에는 아래 요구사항에 맞는 메소드를 정의하고, 
 // main()메소드에 해당 메소드를 호출해서 결과를 확인해보세요. 메소드의 이름도 JAVA의 규칙에 맞게 정의해 봅시다. 
 //① 독감예방 접종이 가능한지 여부를 확인하는 메소드를 정의합니다.
@@ -14,8 +16,58 @@ package ch04;
 
 public class Member {
 	
+	String name;
+	int birthYear;
+	Calendar today = Calendar.getInstance();
+	int thisYear = today.get(Calendar.YEAR);
+	int age;
+	
+	Member() {
+	}
+	Member(int birthYear) {
+		this.birthYear = birthYear;
+	}
+	
+	void nochargeVaccin() {
+		this.age = (thisYear - this.birthYear) + 1;
+		if(age<15 || age>=65) {
+			System.out.println("무료 예방 접종이 가능합니다.");
+		} else {
+			System.out.println("무료 접종 대상이 아닙니다.");
+		}
+	}
+	
+	void checkUp() {
+		age = (thisYear - this.birthYear) + 1;
+		System.out.println("나이:"+age+"세");
+		if(age>=20) {
+			if((birthYear%2==0 && thisYear%2==0) || 
+				(birthYear%2!=0 && thisYear%2!=0)) {
+				System.out.println("올해 건강 검진 대상자입니다.");
+				if(age>=40) {
+					System.out.println("40세 이상이므로 무료 암검진 대상입니다.");
+				}
+			} else {
+				System.out.println("올해 건강 검진 대상자가 아닙니다.");
+			}
+		} else {
+			System.out.println("건강 검진 대상자가 아닙니다.");
+		}
+	}
 	
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("출생 년도 4글자를 입력해주세요.");
+		String input = scanner.nextLine();
+		int birthYear = Integer.parseInt(input);
+		Member member = new Member(birthYear);
 		
+		System.out.println("<무료 독감 예방 접종 대상자 여부>");
+		member.nochargeVaccin();
+		
+		System.out.println();
+		
+		System.out.println("<건강 검진 대상자 여부>");
+		member.checkUp();
 	}
 }
