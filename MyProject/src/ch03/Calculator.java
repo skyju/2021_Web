04695db235e205a23fa9f43eb1986f18f242f3fd
skyju <77267141+skyju@users.noreply.github.com>
2021-05-25 12:12:52 +0900

    Update Calculator.java

--- MyProject/src/ch03/Calculator.java
+++ MyProject/src/ch03/Calculator.java
@@ -2,7 +2,18 @@ package ch03;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-//Calculator 클래스를 정의해봅시다. 
+//Calculator 클래스를 정의해봅시다.
+//① 정수 두 개를 매개변수의 인자로 전달받아 더하기연산 후 반환하는 메소드를 정의
+//② 정수 두 개를 매개변수의 인자로 전달받아 빼기연산 후 반환하는 메소드를 정의
+//③ 정수 두 개를 매개변수의 인자로 전달받아 곱하기연산 후 반환하는 메소드를 정의
+//④ 정수 두 개를 매개변수의 인자로 전달받아 나누기연산 후 반환하는 메소드를 정의
+//⑤ 실수 반지름 하나를 매개변수의 인자로 전달받아 원의 둘레를 구해 반환하는 메소드를 반환하는 메소드를 정의
+//⑥ 실수 반지름 하나를 매개변수의 인자로 전달받아 원의 넓이를 구해 반환하는 메소드를 반환하는 메소드를 정의
+//원의 둘레 : 2 x π x r , 월의 넓이 : π x r x r
+//⑦ main() 메소드를 정의하고 각각의 메소드를 호출해서 결과를 콘솔에 출력해봅시다.
+//⑧ 콘솔에서 사용자에게 데이터를 받아 메소드를 호출할 때 사용자에게 받은 데이터를 
+//메소드의 매개변수의 인자로 전달하는 코드를 main() 메소드에 추가해봅시다.
+
 public class Calculator {
 	
 	public static String inputValue() {
@@ -11,30 +22,26 @@ public class Calculator {
 		return input;
 	}
 	
-	//① 정수 두 개를 매개변수의 인자로 전달받아 더하기연산 후 반환하는 메소드를 정의
-	//② 정수 두 개를 매개변수의 인자로 전달받아 빼기연산 후 반환하는 메소드를 정의
-	//③ 정수 두 개를 매개변수의 인자로 전달받아 곱하기연산 후 반환하는 메소드를 정의
-	//④ 정수 두 개를 매개변수의 인자로 전달받아 나누기연산 후 반환하는 메소드를 정의
 	static int add(int x, int y) {
 		int z = x+y;
 		return z;
 	}
+	
 	static int substract(int x, int y) {
 		int z = x-y;
 		return z;
 	}
+	
 	static int multiply(int x, int y) {
 		int z = x*y;
 		return z;
 	}
+	
 	static double divide(int x, int y) {
 		double z = x/y;
 		return z;
 	}
 	
-	//⑤ 실수 반지름 하나를 매개변수의 인자로 전달받아 원의 둘레를 구해 반환하는 메소드를 반환하는 메소드를 정의
-	//⑥ 실수 반지름 하나를 매개변수의 인자로 전달받아 원의 넓이를 구해 반환하는 메소드를 반환하는 메소드를 정의
-	//원의 둘레 : 2 x π x r , 월의 넓이 : π x r x r
 	static double circumference(double x) {
 		double y = 2*x*3.141;
 		return y;
@@ -46,10 +53,6 @@ public class Calculator {
 	}
 	
 	public static void main(String[] args) {
-		
-	//⑦ main() 메소드를 정의하고 각각의 메소드를 호출해서 결과를 콘솔에 출력해봅시다.
-	//⑧ 콘솔에서 사용자에게 데이터를 받아 메소드를 호출할 때 사용자에게 받은 데이터를 
-	//메소드의 매개변수의 인자로 전달하는 코드를 main() 메소드에 추가해봅시다.
 		
 		try {
 		System.out.println("연산할 두 값을 입력해주세요.");
