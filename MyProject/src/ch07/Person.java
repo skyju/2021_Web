396a2fde36948fe5bf1aa273d72ab7ccfacc189c
skyju <77267141+skyju@users.noreply.github.com>
2021-05-26 09:49:21 +0900

    Update Person.java

--- MyProject/src/ch07/Person.java
+++ MyProject/src/ch07/Person.java
@@ -5,11 +5,17 @@ import java.util.Calendar;
 public class Person {
 	String name;
 	String idNum;
-	int thisYear = Calendar.getInstance().get(Calendar.YEAR);
+	int thisYear;
+	int BirthYear;
+	int age;
 	char[] a = idNum.toCharArray();
-	int BirthYear = 1900+(a[0]-48)*10+(a[1]-48);
-	int age = thisYear - BirthYear + 1;
-
+	
+	void ageCal() {
+		thisYear = Calendar.getInstance().get(Calendar.YEAR);
+		BirthYear = 1900+(a[0]-48)*10+(a[1]-48);
+		age = thisYear - BirthYear + 1;
+	}
+	
 	void hello() {
 		System.out.println("안녕하세요. 저는"+name+"입니다."+
 				age+"살 입니다.");
