cbd281b1d4dfd1929dc5cdade9da30a1ced13124
skyju <77267141+skyju@users.noreply.github.com>
2021-05-26 16:26:52 +0900

    Update Driver.java

--- MyProject/src/ch07/Driver.java
+++ MyProject/src/ch07/Driver.java
@@ -9,10 +9,13 @@ public class Driver {
 	}
 	public static void main(String[] args) {
 		Female person1 = new Female("홍길순", "9209052745687");
+		person1.hello();
 		System.out.println("================");
 		Male person2 = new Male("홍길동", "0305023789564");
+		person2.hello();
 		System.out.println("================");
 		Person person3 = new Person("아무개", "8806081889566");
+		person3.hello();
 	}
 
 }
