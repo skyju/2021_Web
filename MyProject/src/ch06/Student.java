407859447a40d906fe43031916aeb98d3ba7b735
skyju <77267141+skyju@users.noreply.github.com>
2021-05-26 11:47:17 +0900

    Update Student.java

--- MyProject/src/ch06/Student.java
+++ MyProject/src/ch06/Student.java
@@ -1,5 +1,16 @@
 package ch06;
 
+// 배열 > 변수의 집합: 변수를 묶음으로 사용
+// 	왜 사용? 사용해야하는 같은 타입, 같은 성격의 변수가 많을 때 사용
+// 	class도 타입이다 > 참조형 타입
+// 	참조형 타입의 배열 : 참조변수?인스턴스의 주조를 저장 > 객체 배열
+
+// 배열 선언 방법 >
+// 	1차원 배열: 타입[] 식별자(배열이름) = new 타입[size]
+// 	ex) int[] score = new int[10];
+//	score[index] = {};
+// 	index > 주소가 순서대로 연결되어 있기 때문에
+
 public class Student {
 	private String name;
 	private int korScore;
@@ -48,12 +59,12 @@ public class Student {
 	}
 	
 	public void printInfo() {
-		System.out.println(" 이름     : "+getName());
+		System.out.println("  이름  : "+getName());
 		System.out.println("국어 점수: "+getKorScore());
 		System.out.println("영어 점수: "+getEngScor());
 		System.out.println("수학 점수: "+getMathScor());
-		System.out.println(" 총점     : "+totalScore());
-		System.out.println(" 평균     : "+avgScore());
+		System.out.println("  총점  : "+totalScore());
+		System.out.println("  평균  : "+avgScore());
 		System.out.println();
 	}
 	
