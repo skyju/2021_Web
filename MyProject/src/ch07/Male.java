03ef0c0fbe212a95036d859804e697ea7f2df63e
skyju <77267141+skyju@users.noreply.github.com>
2021-05-26 11:03:20 +0900

    Update Male.java

--- MyProject/src/ch07/Male.java
+++ MyProject/src/ch07/Male.java
@@ -1,5 +1,20 @@
 package ch07;
 
 public class Male extends Person {
-	boolean sex;
+	
+	public Male() {
+	}
+	
+	public Male(String name, String idNum) {
+		setName(name);
+		setIdNum(idNum);
+	}
+	
+	void hello() {
+		System.out.println("안녕하세요. 저는"+getName()+"입니다."+
+				getAge()+"살 입니다.");
+		if(!isSex()) {
+			System.out.println("성별은 남성입니다.");
+		}
+	}
 }
