ed0f714fb550804a8cd4f3e59717d5f16e4bad91
skyju <77267141+skyju@users.noreply.github.com>
2021-05-26 11:03:23 +0900

    Update Female.java

--- MyProject/src/ch07/Female.java
+++ MyProject/src/ch07/Female.java
@@ -2,10 +2,20 @@ package ch07;
 
 public class Female extends Person {
 	
-	boolean sex;
+	public Female() {
+	}
 	
-	if(a[7].equals(2)) {
-		sex = true;
+	public Female(String name, String idNum) {
+		setName(name);
+		setIdNum(idNum);
+	}
+	
+	void hello() {
+		System.out.println("안녕하세요. 저는"+getName()+"입니다. "+
+				getAge()+"살 입니다.");
+		if(isSex()) {
+			System.out.println("성별은 여성입니다.");
+		}
 	}
 	
 }
