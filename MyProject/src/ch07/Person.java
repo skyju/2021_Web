433ecc0bd4edea951f6accf12e151d5cf582fb8c
skyju <77267141+skyju@users.noreply.github.com>
2021-05-26 11:03:16 +0900

    Update Person.java

--- MyProject/src/ch07/Person.java
+++ MyProject/src/ch07/Person.java
@@ -3,21 +3,85 @@ package ch07;
 import java.util.Calendar;
 
 public class Person {
-	String name;
-	String idNum;
-	int thisYear;
-	int BirthYear;
-	int age;
-	char[] a = idNum.toCharArray();
+	
+	private String name;
+	private String idNum;
+	int thisYear = Calendar.getInstance().get(Calendar.YEAR);
+	private int BirthYear;
+	private int age;
+	private boolean sex;
+	
+	public Person() {
+	}
+	
+	public Person(String name, String idNum) {
+		this.name = name;
+		this.idNum = idNum;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getIdNum() {
+		return idNum;
+	}
+
+	public void setIdNum(String idNum) {
+		this.idNum = idNum;
+	}
+
+	public int getBirthYear() {
+		return BirthYear;
+	}
+
+	public void setBirthYear(int birthYear) {
+		BirthYear = birthYear;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public boolean isSex() {
+		return sex;
+	}
+
+	public void setSex(boolean sex) {
+		this.sex = sex;
+	}
 	
 	void ageCal() {
-		thisYear = Calendar.getInstance().get(Calendar.YEAR);
-		BirthYear = 1900+(a[0]-48)*10+(a[1]-48);
-		age = thisYear - BirthYear + 1;
+		char[] a = idNum.toCharArray();
+		if(a[6] == '1' || a[6] == '2') {
+			BirthYear = 1900+((a[0]-48)*10+(a[1]-48));
+		} else if(a[6] == '3' || a[6] == '4') {
+			BirthYear = 2000+((a[0]-48)*10+(a[1]-48));
+		} else {
+			System.out.println("올바르지 않은 주민등록번호입니다.");
+		}
+		setAge(thisYear - BirthYear + 1);
+	}
+	
+	void checkSex() {
+		char[] a = idNum.toCharArray();
+		if(a[6] == '2' || a[6] == '4') {
+			setSex(true);
+		} else if(a[6] == '1' || a[6] == '3'){
+			setSex(false);
+		} 
 	}
 	
 	void hello() {
-		System.out.println("안녕하세요. 저는"+name+"입니다."+
-				age+"살 입니다.");
+		System.out.println("안녕하세요. 저는"+getName()+"입니다. "+
+				getAge()+"살 입니다.");
 	}
 }
