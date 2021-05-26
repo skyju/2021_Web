d629355bb108eb58a67da641e3958d7aa1c457e6
skyju <77267141+skyju@users.noreply.github.com>
2021-05-26 11:21:37 +0900

    Update MemberMain.java

--- MyProject/src/ch05/MemberMain.java
+++ MyProject/src/ch05/MemberMain.java
@@ -6,6 +6,8 @@ public class MemberMain {
 		Member member1 = new Member("홍길동", "010-1234-4567", "조리", 4, "asd@asd.com");
 		Member member2 = new Member("홍길금", "010-1234-7890", "경영", 3, "zxc@asd.com", "1998-07-06", "서울");
 		member1.printInfo();
+		
+		member2.setName("영희");
 		member2.printInfo();
 	}
 }
