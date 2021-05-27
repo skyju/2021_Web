84ee2b94dfbfdbe7d4eb1df943b44e2d06a2ed9d
skyju <77267141+skyju@users.noreply.github.com>
2021-05-27 14:46:52 +0900

    Create ArrayQ3.java

---
+++ FirstJava/src/array/ArrayQ3.java
@@ -0,0 +1,33 @@
+package array;
+
+public class ArrayQ3 {
+	
+	public static int[][] rotate(int[][] arr) {
+		int[] tmp = arr[arr.length-1];
+		for(int j = arr.length-1 ; j > 0 ; j --) {
+			arr[j] = arr[j-1];
+		}
+		arr[0] = tmp;	
+		return arr;
+	}
+	
+	public static void main(String[] args) {
+		
+		int arr[][] = {
+				{1,2,3},
+				{4,5,6},
+				{7,8,9}
+		};
+		
+		rotate(arr);
+		
+		for(int[] a : arr) {
+			for(int b : a) {
+				System.out.print(b+",");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
