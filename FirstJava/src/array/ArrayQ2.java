d94e536d2b01349aa289330187d60d72200da7ac
skyju <77267141+skyju@users.noreply.github.com>
2021-05-27 14:46:55 +0900

    Create ArrayQ2.java

---
+++ FirstJava/src/array/ArrayQ2.java
@@ -0,0 +1,40 @@
+package array;
+
+public class ArrayQ2 {
+	public static void addOneDArr(int[] arr, int add) {
+		for(int i = 0 ; i < arr.length ; i++) {
+			arr[i] += add;
+		}
+	}
+	
+	public static void addOneDArr2(int[][] arr, int add) {
+		for(int i = 0 ; i < arr.length ; i ++) {
+			for(int j = 0 ; j < arr[i].length ; j ++) {
+				arr[i][j] += add;
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		int arr[][] = {
+				{1,2,3,4,5},
+				{2,3,4,5,6},
+				{3,4,5,6,7},
+				{4,5,6,7,8},
+				{5,6,7,8,9}
+		};
+		
+		addOneDArr2(arr, 2);
+		
+		for(int[] a : arr) {
+			for(int b : a) {
+				System.out.print(b+",");
+			}
+		System.out.println();
+		}
+		
+		
+	}
+
+}
