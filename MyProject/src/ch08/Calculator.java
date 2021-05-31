618f8e21751fa10faf03ba3a058e6563ffb1cb5d
skyju <77267141+skyju@users.noreply.github.com>
2021-05-31 13:20:53 +0900

    Update Calculator.java

--- MyProject/src/ch08/Calculator.java
+++ MyProject/src/ch08/Calculator.java
@@ -1,5 +1,7 @@
 package ch08;
 
+import java.util.*;
+
 public class Calculator extends ACalculator {
 
 	@Override
@@ -21,15 +23,25 @@ public class Calculator extends ACalculator {
 	public double divide(double n1, double n2) {
 		return n1/n2;
 	}
-
+	
 	@Override
 	public void showResult() {
-		System.out.println("result:");
+		Scanner kb = new Scanner(System.in);
+		System.out.println("첫 번째 수를 입력해주세요.");
+		long Value1 = kb.nextInt();
+		System.out.println("두 번째 수를 입력해주세요.");
+		long Value2 = kb.nextInt();
+		System.out.println("add result: "+add(Value1, Value2));
+		System.out.println("substract result: "+substract(Value1, Value2));
+		System.out.println("multiply result: "+multiply(Value1, Value2));
+		System.out.println("divide result: "+divide(Value1, Value2));
 	}
-
+	
 	public static void main(String[] args) {
+		ICalculator cal1 = new Calculator();
+		ACalculator cal2 = new Calculator();
 		
-
+		((Calculator) cal1).showResult();
 	}
 
 }
