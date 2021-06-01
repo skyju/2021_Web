be699e9b2d879288889dbc5022c584cb4fff7740
skyju <77267141+skyju@users.noreply.github.com>
2021-06-01 14:01:54 +0900

    Create Q.java

---
+++ MyProject/src/ch10/Q.java
@@ -0,0 +1,16 @@
+package ch10;
+
+import java.util.*;
+
+public class Q {
+	public String getUserInput() {
+		Scanner kb = new Scanner(System.in);
+		String input = kb.nextLine();
+		return input;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
