ecb735c10c415714e8730bf3a12e963f145eed84
skyju <77267141+skyju@users.noreply.github.com>
2021-06-01 14:42:18 +0900

    Update Q2.java

--- MyProject/src/ch10/Q2.java
+++ MyProject/src/ch10/Q2.java
@@ -12,7 +12,11 @@ public class Q2 {
 	
 	public static void main(String[] args) {
 		try {
+			System.out.println("태어난 년도를 입력해주세요.");
 			int birth = getUserInput();
+			if(birth < 1900 || birth > 2021) {
+				throw new BadIdInputException("잘못된 년도입니다.");
+			}
 		} catch (InputMismatchException e) {
 			System.out.println("숫자가 아닙니다.");
 		} catch (Exception e) {
