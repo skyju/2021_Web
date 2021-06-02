6448f9195a818aeb993d0f1994b7f62b1b4e9184
skyju <77267141+skyju@users.noreply.github.com>
2021-06-02 12:35:50 +0900

    commit

--- FirstJava/src/ex/io/ByteBufferedFileCopy.java
+++ FirstJava/src/ex/io/ByteBufferedFileCopy.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class BufferFileCopy2 {
+public class ByteBufferedFileCopy {
 
 	public static void main(String[] args) {
 		try {
