ec433c675c9bc8d3010682095549bec985375e18
skyju <77267141+skyju@users.noreply.github.com>
2021-06-04 14:00:43 +0900

    Update Q2.java

--- MyProject/src/ch13/Q2.java
+++ MyProject/src/ch13/Q2.java
@@ -4,38 +4,40 @@ import java.util.*;
 
 public class Q2 {
 	
+	static String exit = "";
+	
 	public static void main(String[] args) {
 		
-		File originFile;
-		File newDir;
 		Scanner kb = new Scanner(System.in);
-		
 		System.out.println("[알림] 파일 복사 프로그램을 실행합니다.");
-		System.out.println("[입력] 복사할 파일의 경로를 입력해주세요"
-				+ "\n      (확장자 명까지 입력해주세요.): ");
 		
-		//원본 파일 객체 생성
-		originFile = new File(kb.nextLine());
-		
-		if(!originFile.exists()) {
-			System.out.println("[알림] 입력 된 경로에 원본 파일이 준비되어 있지 않습니다.");
-			System.out.println("      프로그램을 종료합니다.");
-			return;
+		while(exit != "exit") {
+			System.out.println("[입력] 복사할 파일의 경로를 입력해주세요"
+					+ "\n      (확장자 명까지 입력해주세요.): ");
+			
+			//원본 파일 객체 생성
+			File originFile = new File(kb.nextLine());
+			
+			if(!originFile.exists()) {
+				System.out.println("[알림] 입력 된 경로에 원본 파일이 준비되어 있지 않습니다."
+						+ "\n      다시 입력해주세요.");
+				continue;
+			}
+			
+			System.out.println("[입력] 파일을 옮길 새로운 경로를 입력해주세요");
+			
+			//새로운 경로 설정
+			File newDir = new File(kb.nextLine());
+			
+			if(!newDir.exists()) {
+				System.out.println("[알림] 올바르지 않은 경로입니다."
+						+ "\n      다시 입력해주세요.");
+				continue;
+			}
+			
+			copyFileThread ct = new copyFileThread(originFile, newDir);
+			ct.start();
 		}
-		
-		System.out.println("[입력] 파일을 옮길 새로운 경로를 입력해주세요");
-		
-		//새로운 경로 설정
-		newDir = new File(kb.nextLine());
-		
-		if(!newDir.exists()) {
-			System.out.println("[알림] 올바르지 않은 경로입니다.");
-			System.out.println("      프로그램을 종료합니다.");
-			return;
-		}
-		
-		copyFileThread ct = new copyFileThread(originFile, newDir);
-		ct.start();
 	}
 
 }
@@ -76,6 +78,11 @@ class copyFileThread extends Thread {
 			
 			System.out.println("[알림] 복사완료!"
 					+ "\n      복사된 바이트 사이즈 :"+copyByteSize+" byte");
+			
+			Scanner kb = new Scanner(System.in);
+			System.out.print("[알림] 종료하려면 exit을, "
+					+ "\n      계속 진행하시려면 복사할 파일의 경로를 입력해주세요.");
+			Q2.exit = kb.nextLine();
 			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
