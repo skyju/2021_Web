4b47a69fa8ffff1bbd7901e327298ffa22754db0
skyju <77267141+skyju@users.noreply.github.com>
2021-06-10 18:20:49 +0900

    Update REGULAR_EXPRESSION.sql

--- SQL/DAY03/REGULAR_EXPRESSION.sql
+++ SQL/DAY03/REGULAR_EXPRESSION.sql
@@ -1,5 +1,6 @@
 
 -- 정규 표현식 (REGULAR EXPRESSION - REGEXP_)
+-- https://ko.wikipedia.org/wiki/%EC%A0%95%EA%B7%9C_%ED%91%9C%ED%98%84%EC%8B%9D 참고
 
 -- (1) REGEXP_LIKE(문자열, 패턴, ...) : LIKE과 유사, 정규식을 사용할 수 있는 LIKE
 -- (2) REGEXP_COUNT: 패턴에 일치하는 문자열 갯수 카운트
@@ -7,7 +8,13 @@
 -- (4) REGEXP_SUBSTR: 문자열 내에서 패턴에 일치하는 문자열을 추출
 -- (5) REGEXP_REPLACE: 문자열 내에서 패턴에 일치하는 문자열을 다른 문자열로 대체
 
--- 유의할 점, or을 대체하는 |는 되지만, &는 정규표현식에 없음
+-- [:DIGIT] 숫자의 묶음을 나타내는 숫자 클래스 0~9
+-- [:ALPHA] 알파벳을 뜻하는 문자 클래스
+-- [:UPPER] 대문자
+-- [:LOWER] 소문자
+-- [:ALNUM] 대소문자 구분하지 않고, 영숫자
+
+-- 유의할 점: or을 대체하는 |는 되지만, &는 정규표현식에 없음
 
 
 -- (1) REGEXP_LIKE(source_char, pattern [, match_param])
@@ -16,6 +23,10 @@
 SELECT ENAME
 FROM EMP
 WHERE REGEXP_LIKE(ENAME, '^A');
+
+SELECT ENAME
+FROM EMP
+WHERE REGEXP_LIKE(ENAME, '^C.*');
 
 SELECT ENAME
 FROM EMP
@@ -39,7 +50,7 @@ WHERE REGEXP_LIKE(ENAME, 'A|B|C');
 
 SELECT ENAME
 FROM EMP
-WHERE REGEXP_LIKE(ENAME, 'a|b|c', 'i');
+WHERE REGEXP_LIKE(ENAME, 'a|b|c', 'i'); -- 대소문자 무시는 정규식 사용해야 가능
 
 -- 특정문자로 시작하고, 한문자 건너 띄고, 다음문자가 특정문자인 문자열 찾기
 SELECT ENAME
@@ -55,3 +66,18 @@ WHERE REGEXP_LIKE(ENAME, 'L{2}'); --  중괄호 써야 함 주의
 SELECT ENAME
 FROM EMP
 WHERE REGEXP_LIKE(ENAME, '(LE)\1');
+
+
+
+-- (2) REGEXP_COUNT: 패턴에 일치하는 문자열 갯수 카운트
+                    -- 기존의 COUNT()함수는 ROW갯수를 세는 함수고, 
+                    -- 정규표현식 COUNT는 하나의 문자열 내에서 반복문자열의 갯수를 셈
+                    -- 예를들어 긍정적 표현이 몇개 있는지 COUNT
+                    
+-- REGEXP_COUNT(soure_char, pattern [,position [, match_param]])
+
+-- 전화번호 포맷이 999.999.999와 일치하는 데이터 찾기
+SELECT PHONE_NUMBER
+FROM HR.employees
+WHERE REGEXP_COUNT(PHONE_NUMBER, '()') = 1 ;
+
