94db8d1bfa803223841439027f3cb7859429d3c6
skyju <77267141+skyju@users.noreply.github.com>
2021-06-10 17:58:58 +0900

    Create REGULAR_EXPRESSION.sql

---
+++ SQL/DAY03/REGULAR_EXPRESSION.sql
@@ -0,0 +1,57 @@
+
+-- 정규 표현식 (REGULAR EXPRESSION - REGEXP_)
+
+-- (1) REGEXP_LIKE(문자열, 패턴, ...) : LIKE과 유사, 정규식을 사용할 수 있는 LIKE
+-- (2) REGEXP_COUNT: 패턴에 일치하는 문자열 갯수 카운트
+-- (3) REGEXP_INSTR: 문자열 내에서 패턴에 일치하는 시작 위치를 찾아줌
+-- (4) REGEXP_SUBSTR: 문자열 내에서 패턴에 일치하는 문자열을 추출
+-- (5) REGEXP_REPLACE: 문자열 내에서 패턴에 일치하는 문자열을 다른 문자열로 대체
+
+-- 유의할 점, or을 대체하는 |는 되지만, &는 정규표현식에 없음
+
+
+-- (1) REGEXP_LIKE(source_char, pattern [, match_param])
+
+-- 특정문자로 시작하는 문자열 찾기
+SELECT ENAME
+FROM EMP
+WHERE REGEXP_LIKE(ENAME, '^A');
+
+SELECT ENAME
+FROM EMP
+WHERE ENAME LIKE 'A%';
+
+-- 특정문자로 끝나는 문자열 찾기
+SELECT ENAME
+FROM EMP
+WHERE REGEXP_LIKE(ENAME, 'N$');
+
+SELECT ENAME
+FROM EMP
+WHERE ENAME LIKE '%N';
+
+-- 특정문자가 들어있는 문자열 찾기 
+-- (대소문자 구분은 ,'C'): case sensitive
+-- (대소문자 무시는 ,'i'): ignore
+SELECT ENAME
+FROM EMP
+WHERE REGEXP_LIKE(ENAME, 'A|B|C');
+
+SELECT ENAME
+FROM EMP
+WHERE REGEXP_LIKE(ENAME, 'a|b|c', 'i');
+
+-- 특정문자로 시작하고, 한문자 건너 띄고, 다음문자가 특정문자인 문자열 찾기
+SELECT ENAME
+FROM EMP
+WHERE REGEXP_LIKE(ENAME, '^[B-Z].I');
+
+-- 특정문자가 N번 반복되는 문자열 찾기
+SELECT ENAME
+FROM EMP
+WHERE REGEXP_LIKE(ENAME, 'L{2}'); --  중괄호 써야 함 주의
+
+-- 패턴이 반복되는 경우 \1을 여러번 사용하여 처리
+SELECT ENAME
+FROM EMP
+WHERE REGEXP_LIKE(ENAME, '(LE)\1');
