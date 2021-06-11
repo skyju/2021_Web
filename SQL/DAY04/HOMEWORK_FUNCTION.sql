ad3af4ce92036d62b8c36ea4fab583cd920828f4
skyju <77267141+skyju@users.noreply.github.com>
2021-06-11 17:18:32 +0900

    Update HOMEWORK_FUNCTION.sql

--- SQL/DAY04/HOMEWORK_FUNCTION.sql
+++ SQL/DAY04/HOMEWORK_FUNCTION.sql
@@ -92,7 +92,6 @@ GROUP BY DEPTNO;
 -- 30. 각 부서에 대해 부서번호 이름, 지역 명, 사원 수, 부서내의 모든 사원의 평균 급여를 출력하시오. 
 -- 평균 급여는 정수로 반올림 하시오. DECODE 사용.
 
-
 -- DECODE 사용하여 풀이
 SELECT DEPTNO, DECODE(DEPTNO, 10, 'ACCOUNTING',
                               20, 'RESEARCH',
