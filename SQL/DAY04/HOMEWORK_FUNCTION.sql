27db9466e9b766fdbfd3461403efc0a24b7d7984
skyju <77267141+skyju@users.noreply.github.com>
2021-06-11 14:57:17 +0900

    COMMIT

---
+++ SQL/DAY04/HOMEWORK_FUNCTION.sql
@@ -0,0 +1,129 @@
+-- 16. SUBSTR 함수를 사용하여 사원들의 입사한 년도와 입사한 달만 출력하시오.
+
+SELECT REGEXP_SUBSTR(HIREDATE, '^\d\d\/\d\d')
+FROM EMP;
+
+-- 17. SUBSTR 함수를 사용하여 4월에 입사한 사원을 출력하시오.
+
+SELECT *
+FROM EMP
+WHERE REGEXP_SUBSTR(HIREDATE, '\d', 4) = 4;
+
+-- 18. MOD 함수를 사용하여 사원번호가 짝수인 사람만 출력하시오.
+
+SELECT *
+FROM EMP
+WHERE MOD(EMPNO,2) = 0;
+
+-- 19. 입사일을 년도는 2자리(YY), 월은 숫자(MM)로 표시하고 요일은 약어 (DY)로 지정하여 출력하시오.
+
+SELECT TO_CHAR(HIREDATE, 'YY-MM. DY')
+FROM EMP
+ORDER BY HIREDATE;
+
+-- 20. 올해 몇 칠이 지났는지 출력하시오. 
+-- 현재날짜에서 올해 1월 1일을 뺀 결과를 출력하고 TO_DATE 함수를 사용하여 데이터 형을 일치 시키시오.
+
+SELECT SYSDATE - (TO_DATE('2021/01/01', 'YYYY/MM/DD'))
+FROM DUAL;
+
+-- 21. 사원들의 상관 사번을 출력하되 상관이 없는 사원에 대해서는 NULL 값 대신 0으로 출력하시오.
+
+SELECT EMPNO, NVL(MGR,0)
+FROM EMP;
+
+-- 22. DECODE 함수로 직급에 따라 급여를 인상하도록 하시오. 
+-- 직급이 ‘ANALIST”인 사원은 200, ‘SALESMAN’인 사원은 180, ‘MANAGER’인 사원은 150, ‘CLERK”인 사원은 100을 인상하시오.
+
+SELECT EMPNO, SAL, DECODE(
+    JOB, 'ANALIST', SAL+200,
+         'SALESMAN', SAL+180,
+         'MANAGER', SAL+150,
+         'CLERK', SAL+100) AS ADDSAL
+FROM EMP;
+
+-- 23. 모든 사원의 급여 최고액, 최저액, 총액 및 평균 급여를 출력하시오. 평균에 대해서는 정수로 반올림하시오.
+
+SELECT MAX(SAL), MIN(SAL), SUM(SAL), TRUNC(AVG(SAL)) AS AVG_SAL
+FROM EMP;
+​
+-- 24. 각 담당 업무 유형별로 급여 최고액, 최저액, 총액 및 평균 액을 출력하시오. 평균에 대해서는 정수로 반올림 하시오.
+
+SELECT JOB, MAX(SAL), MIN(SAL), SUM(SAL), TRUNC(AVG(SAL)) AS AVG_SAL
+FROM EMP
+GROUP BY JOB;
+
+-- 25. COUNT(*) 함수를 이용하여 담당업무가 동일한 사원 수를 출력하시오.
+
+SELECT JOB, COUNT(*) AS "담당업무 사원 수"
+FROM EMP
+GROUP BY JOB;
+
+-- 26. 관리자 수를 출력하시오.
+
+SELECT COUNT(DISTINCT MGR)
+FROM EMP;
+​
+-- 27. 급여 최고액, 급여 최저액의 차액을 출력하시오.
+
+SELECT MAX(SAL)-MIN(SAL)
+FROM EMP;
+
+-- 28. 직급별 사원의 최저 급여를 출력하시오. 
+-- 관리자를 알 수 없는 사원과 최저 급여가 2000 미만인 그룹은 제외시키고 
+-- 결과를 급여에 대한 내림차순으로 정렬하여 출력하시오.
+
+SELECT JOB, MIN(SAL)
+FROM EMP
+WHERE MGR IS NOT NULL
+GROUP BY JOB
+HAVING MIN(SAL) >= 2000
+ORDER BY MIN(SAL) DESC;
+
+-- 29. 각 부서에 대해 부서번호, 사원 수, 부서 내의 모든 사원의 평균 급여를 출력하시오. 
+-- 평균 급여는 소수점 둘째 자리로 반올림 하시오.
+
+SELECT DEPTNO, COUNT(*) AS "사원 수", ROUND(AVG(SAL),2) AS "부서 별 평균 급여"
+FROM EMP
+GROUP BY DEPTNO;
+
+-- 30. 각 부서에 대해 부서번호 이름, 지역 명, 사원 수, 부서내의 모든 사원의 평균 급여를 출력하시오. 
+-- 평균 급여는 정수로 반올림 하시오. DECODE 사용.
+
+
+-- DECODE 사용하여 풀이
+SELECT DEPTNO, DECODE(DEPTNO, 10, 'ACCOUNTING',
+                              20, 'RESEARCH',
+                              30, 'SALES',
+                              40, 'OPERATIONS') AS DNAME,
+               DECODE(DEPTNO, 10, 'NEW YORK',
+                              20, 'DALLAS',
+                              30, 'CHICAGO',
+                              40, 'BOSTON') AS LOC,
+               COUNT(*) AS EMPNUM, ROUND(AVG(SAL),0) AS AVGSAL
+FROM EMP
+GROUP BY DEPTNO;
+
+-- 31. 업무를 표시한 다음 해당 업무에 대해 부서 번호별 급여 및 부서 10, 20, 30의 급여 총액을 각각 출력하시오. 
+-- 별칭은 각 job, dno, 부서 10, 부서 20, 부서 30, 총액으로 지정하시오. 
+-- ( hint. Decode, group by )
+
+-- 별칭 안넣고 풀이
+SELECT JOB, DEPTNO, SUM(SAL)
+FROM EMP
+GROUP BY JOB, DEPTNO
+ORDER BY DEPTNO;
+
+-- 별칭 넣고 풀이
+SELECT JOB, DEPTNO AS DNO,
+            DECODE(DEPTNO, 10, SUM(SAL)) AS "부서 10",
+            DECODE(DEPTNO, 20, SUM(SAL)) AS "부서 20",
+            DECODE(DEPTNO, 30, SUM(SAL)) AS "부서 30",
+            SUM(SAL) AS "총액"
+FROM EMP
+GROUP BY JOB, DEPTNO
+ORDER BY DEPTNO;
+            
+
+
+​
--- SQL/DAY04/다중행함수.sql
+++ SQL/DAY04/다중행함수.sql
@@ -4,10 +4,10 @@
 -- 4. 최대 급여 - max
 -- 5. 최소 급여 - min
 -- 6. 그룹함수는 null값을 제외하고 연산한다.
+-- 7. group by
+-- 8. group by & having
 
 -- 결과가 단일행이므로, 앞에 일반 컬럼을 같이 고르지 못함
-
-
 
 -- 1. 사원의 수
 select count(*) as numberOfemp
@@ -35,4 +35,62 @@ select sum(comm) as "커미션의 총합",
 from emp;
 
 
+
+-- 7.
+-- 특정 컬럼을 이용해서 그룹핑하기 > group by
+-- select 그룹핑할 기준의 컬럼, 그룹함수 
+-- from table 
+-- where 
+-- group by 그룹핑할 기준의 칼럼
+
+
+-- 부서별 사원 수
+select deptno, count(*)
+from emp
+group by deptno;
+
+-- 부서별 연봉의 합계, 평균, 최대, 최소
+select deptno, count(*) as "부서인원", sum(sal), trunc(avg(sal)), max(sal), min(sal)
+from emp
+group by deptno;
+
+
+-- distict와 같은
+select distinct job
+from emp;
+
+select job
+from emp
+group by job;
+
+-- 부서 별로 사원 수와 커미션을 받는 사원들의 수를 계산하여 출력하라
+select deptno, count(*), count(comm)
+from emp
+group by deptno;
+
+
+select count(*), count(comm)
+from emp
+where deptno = 10;
+
+
+-- 8. 
+-- select 그룹핑할 기준의 컬럼, 그룹함수 
+-- from table 
+-- where 
+-- group by 그룹핑할 기준의 칼럼
+-- having 그룹함수 (=, !=, >, <, <=, >=)
+
+select deptno, avg(sal)
+from emp
+group by deptno
+having avg(sal) >= 2000;
+
+-- 부서별 급여의 최대값과 최소값을 구하되,
+-- 최대 급여가 2900이상인 부서만 출력하라.
+select deptno, max(sal), min(sal)
+from emp
+group by deptno
+having max(sal) >= 2900
+order by deptno;
 
