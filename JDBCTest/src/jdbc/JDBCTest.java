d2b6e6722aba5e1129fb7255cecb845ba6cf4fdb
skyju <77267141+skyju@users.noreply.github.com>
2021-06-18 11:03:48 +0900

    Update JDBCTest.java

--- JDBCTest/src/jdbc/JDBCTest.java
+++ JDBCTest/src/jdbc/JDBCTest.java
@@ -1,10 +1,33 @@
 package jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JDBCTest {
 
 	public static void main(String[] args) {
-		// 1. 드라이버 로드
-		class.forName();
+		System.out.println("test start");
+		try {
+			// 1. 드라이버 로드
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			System.out.println("드라이버 로드 성공!");
+			
+			// 2. 연결
+			String jdbcUrl = "jdbc:oracle:thin:@localhost:1521:xe";
+			String user = "hr";
+			String pw = "tiger";
+			
+			Connection con = DriverManager.getConnection(jdbcUrl, user, pw);
+			System.out.println("DB에 접속했습니다.");
+			con.close();
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 클래스를 찾지 못함!");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
