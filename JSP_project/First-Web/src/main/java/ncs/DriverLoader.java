ed9ffb8066ae94e6f1d8cd030a2e0c54f751e28a
skyju <77267141+skyju@users.noreply.github.com>
2021-07-22 10:45:09 +0900

    commit

--- JSP_project/First-Web/src/main/java/ncs/DriverLoader.java
+++ JSP_project/First-Web/src/main/java/ncs/DriverLoader.java
@@ -1,18 +1,23 @@
 package ncs;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
 public class DriverLoader extends HttpServlet {
 	
 	@Override
-	public void init() throws ServletException {
-		// 데이터베이스 드라이버 로드
+	public void init(ServletConfig config) throws ServletException {
+		String param = config.getInitParameter("driver");
+		
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-
-			System.out.println("드라이버 로드 성공!");
-
+			if (param != null ) {
+				Class.forName(param);
+				System.out.println("드라이버 로드 성공!");
+			} else {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				System.out.println("드라이버 로드 성공!");
+			}
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로드 실패!");
 		}
