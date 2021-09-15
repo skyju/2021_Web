dcece66a6651614b19979e3e2363849ead5039b8
skyju <77267141+skyju@users.noreply.github.com>
2021-09-15 16:44:16 +0900

    commit~

---
+++ Spring_project/Orl-c/src/main/java/com/bitcamp/orl/crew/service/CrewManageService.java
@@ -0,0 +1,123 @@
+package com.bitcamp.orl.crew.service;
+
+import java.io.File;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bitcamp.orl.crew.dao.Dao;
+import com.bitcamp.orl.crew.domain.Crew;
+import com.bitcamp.orl.crew.domain.CrewInsertRequest;
+import com.bitcamp.orl.member.domain.MemberDto;
+
+@Service
+public class CrewManageService {
+	
+	final String UPLOAD_URI ="/images/crew";
+	
+	Dao dao;
+	
+	@Autowired
+	SqlSessionTemplate template;
+	
+	public boolean isHaveAuth(
+			int crewIdx,
+			HttpServletRequest request
+			) {
+		dao = template.getMapper(Dao.class);
+		Crew crew = dao.selectCrew(crewIdx);
+		boolean chk = false;
+		MemberDto dto = (MemberDto)request.getSession().getAttribute("memberVo");
+		try {
+			int nowAuthIdx = dto.getMemberIdx();
+			int crewAuthIdx = crew.getMemberIdx();
+			if(nowAuthIdx == crewAuthIdx) {
+				chk = true;
+			}
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+		}
+		return chk;
+	}
+	
+	public Crew selectCrew(int crewIdx) {
+		return template.getMapper(Dao.class).selectCrew(crewIdx);
+	}
+	
+	public File selectThatFile(
+			HttpServletRequest request,
+			int crewIdx
+			) {
+		dao = template.getMapper(Dao.class);
+		String path = request.getSession().getServletContext().getRealPath(UPLOAD_URI);
+		File Dir = new File(path);
+		File file = null;
+		Crew crew = dao.selectCrew(crewIdx);
+		System.out.println(crew);
+		try {
+			String crewPhoto = crew.getCrewPhoto();
+			file = new File(Dir, crewPhoto);
+		} catch(NullPointerException e) {
+			e.printStackTrace();
+		}
+		return file;
+	}
+	
+	public int updateCrew(
+			CrewInsertRequest crewRequest,
+			HttpServletRequest request,
+			int crewIdx
+			) {
+		int resultCnt = 0;
+		File newFile = null;
+		dao = template.getMapper(Dao.class);
+		
+		CrewInsertService insertservice = new CrewInsertService();
+		try {
+			if (crewRequest.getCrewPhoto() != null && !crewRequest.getCrewPhoto().isEmpty()) {
+				newFile = insertservice.saveFile(request, crewRequest.getCrewPhoto());
+				selectThatFile(request, crewIdx).delete();
+				resultCnt = dao.updateCrew(
+						crewRequest.getCrewName(), 
+						newFile.getName(), 
+						crewRequest.getCrewDiscription(), 
+						crewRequest.getCrewTag(),
+						crewIdx);
+			} else {
+				resultCnt = dao.updateCrewWithoutPhoto(
+						crewRequest.getCrewName(), 
+						crewRequest.getCrewDiscription(), 
+						crewRequest.getCrewTag(),
+						crewIdx);
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+			if(newFile != null && newFile.exists()) {
+				newFile.delete();
+				System.out.println("오류가 발생하여 파일을 삭제했습니다.");
+			}
+		}
+		return resultCnt;
+	}
+	
+	public int deleteCrew(
+			int crewIdx,
+			String crewName,
+			HttpServletRequest request
+			) {
+		int resultCnt = 0;
+		dao = template.getMapper(Dao.class);
+		Crew crew = dao.selectCrew(crewIdx);
+		if(crew.getCrewName().contentEquals(crewName)) {
+			resultCnt = dao.deleteCrew(crewIdx);
+			File thatFile = selectThatFile(request, crewIdx);
+			if(thatFile != null) {
+				thatFile.delete();
+			}
+		}
+		return resultCnt;
+	}
+}
