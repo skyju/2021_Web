56d861f9729ec29088759c63a7cf7b37198ce21a
skyju <77267141+skyju@users.noreply.github.com>
2021-09-27 18:49:40 +0900

    ..

--- Spring_project/Orl-Crew/src/main/java/com/bitcamp/orl/crew/controller/RestCrewListController.java
+++ Spring_project/Orl-Crew/src/main/java/com/bitcamp/orl/crew/controller/RestCrewListController.java
@@ -1,5 +1,7 @@
 package com.bitcamp.orl.crew.controller;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,58 +22,63 @@ public class RestCrewListController {
 
 	@Autowired
 	CrewListViewService service;
-	
-	//My Crew List 가져오는 method
+
+	// My Crew List 가져오는 method
 	@RequestMapping("/crew/getMyCrewList")
 	@CrossOrigin
 	public List<Crew> getMyCrewList(
-			@RequestParam("memberIdx")int memberIdx
-			){
-		List<Crew> myCrewList =  null;
-		if(memberIdx != 0) {
+			@RequestParam(value = "memberIdx", required = false) int memberIdx) {
+		List<Crew> myCrewList = null;
+		if (memberIdx != 0) {
 			myCrewList = service.getMyCrewList(memberIdx);
 		}
 		return myCrewList;
 	}
-	
-	//Crew List Page에서 Page정보와 List<Crew>객체를 같이 넘겨주는 method
-	@RequestMapping("/crew/searchList")
+
+	// Crew List Page에서 Page정보와 List<Crew>객체를 같이 넘겨주는 method
+	@RequestMapping(value = "/crew/searchList", produces = "application/json; charset=utf8")
 	@CrossOrigin
-	public CrewForPaging getCrewList(
-			@RequestParam(value="searchType", required = false)String searchType,
-			@RequestParam(value="keyword", required = false)String keyword,
-			@RequestParam(value="page", required = false, defaultValue = "1")int page
-			){
-		
-		//PageMaker+List<Crew>담는 객체인 CrewForPaging 생성
+	public CrewForPaging getCrewList(@RequestParam(value = "searchType", required = false) String searchType,
+			@RequestParam(value = "keyword", required = false) String keyword,
+			@RequestParam(value = "page", required = false, defaultValue = "1") int page
+			)throws UnsupportedEncodingException {
+		// PageMaker+List<Crew>담는 객체인 CrewForPaging 생성
 		CrewForPaging crewForPaging = null;
-		//현재 페이지 값을 받아서 페이징에 필요한 값을 계산하기 위한 Criteria 객체 생성
+		// 현재 페이지 값을 받아서 페이징에 필요한 값을 계산하기 위한 Criteria 객체 생성
 		CrewListCriteria cri = new CrewListCriteria(page);
 		
-		//페이징 처리
+		SearchType st;
+
+		if (keyword != null) {
+			String keywordK = URLDecoder.decode(keyword, "UTF-8");
+			System.out.println(keywordK);
+			st = new SearchType(searchType, keywordK, cri.getPageStart(), cri.getPerPageNum());
+		} else {
+			st = new SearchType(searchType, keyword, cri.getPageStart(), cri.getPerPageNum());
+		}
+
+		// 페이징 처리
 		PageMaker pageMaker = new PageMaker();
 		pageMaker.setCri(cri);
-		
-		//총 게시물 수
+
+		// 총 게시물 수
 		int totalNum;
-		
-		SearchType st = new SearchType(searchType, keyword, cri.getPageStart(), cri.getPerPageNum());
-		
+
 		List<Crew> allCrewList;
-		
-		//검색 keyword가 있는 경우
-		if(st.getKeyword() !=null && st.getKeyword().trim().length() > 0) {
-			allCrewList= service.getCrewListAll(st);
+
+		// 검색 keyword가 있는 경우
+		if (st.getKeyword() != null && st.getKeyword().trim().length() > 0) {
+			allCrewList = service.getCrewListAll(st);
 			totalNum = service.getCrewCountForSearching(st);
 			pageMaker.setTotalCount(totalNum);
-		} else { //검색 keyword가 없는 경우
+		} else { // 검색 keyword가 없는 경우
 			allCrewList = service.getCrewListAll(cri.getPageStart(), cri.getPerPageNum());
 			totalNum = service.getCrewCount();
 			pageMaker.setTotalCount(totalNum);
 		}
-		
+
 		crewForPaging = new CrewForPaging(allCrewList, pageMaker);
-		
+
 		return crewForPaging;
 	}
 }
--- Spring_project/Orl-Crew/src/main/java/com/bitcamp/orl/crew/domain/CrewListCriteria.java
+++ Spring_project/Orl-Crew/src/main/java/com/bitcamp/orl/crew/domain/CrewListCriteria.java
@@ -6,6 +6,7 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class CrewListCriteria {
+	
 	private int page; // 보여줄 페이지
     private int perPageNum = 6; // 페이지당 보여줄 페이지수
  
@@ -23,18 +24,9 @@ public class CrewListCriteria {
  
     public void setPage(int page) {
         if ( page <= 0 ){
-            // 페이지는 1페이지부터임으로 0보다 작거나 같은값일 경우 무조건 첫번째 페이지로 설정되도록 해준다.
             this.page = 1;
         }else{
             this.page = page;
-        }
-    }
-    
-    public void setPerPageNum(int perPageNum) {
-        if ( perPageNum <= 0 || perPageNum > 100 ) {
-            this.perPageNum = 10;
-        }else {
-            this.perPageNum = perPageNum;
         }
     }
     
